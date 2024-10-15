9e80143b0c6417e5f9f1096f61679781bf1d8432
Kartikjain2626 <kartikjain2626@gmail.com>
2024-10-15 20:49:05 +0530

    New Commits

---
+++ forofLoop.js
@@ -0,0 +1,14 @@
+// this is the FOR OF LOOP 
+
+// brr = [3,7,0,2]
+// for(const ele of brr){
+//     console.log(ele*2);
+// }
+
+
+//For each loop 
+
+brr = [3,7,0,2]
+brr.forEach(ele => {
+    console.log(ele*2)
+});
